15cb18e9e7c5a6f3301532a3531aba6308ddecd1
Tiffany <TiffanyJWise84@gmail.com>
2021-04-20 14:29:56 -0400

    Corrected formatting for hour changes. Added stop timer

--- src/controllers/Counter.js
+++ src/controllers/Counter.js
@@ -1,75 +1,6 @@
 import {CountDown} from './CountDown.js';
 
-
-// class Counter{
-//     constructor(timeLeft){          
-
-//         this._timeLeft = timeLeft;
-
-//         this._dayElement = document.getElementById("days");
-//         this._hourElement = document.getElementById("hours");
-//         this._minutesElement = document.getElementById("minutes");
-//         this._secondsElement = document.getElementsByName("seconds");
-
-//         // var page = document.getElementsByTagName("body");
-//     }
-
-//     displayValues(){
-//         this._dayElement.innerHTML = this._timeLeft['days'];
-//         this._hourElement.innerHTML = this._timeLeft['hours'];
-//         this._minutesElement.innerHTML = this._timeLeft['minutes'];
-//         this._secondsElement.innerHTML = this._timeLeft['seconds'];
-
-//         // display starting values
-//         console.log(`Values Displayed`);
-//         console.log(`\tDays = ${this._timeLeft['days']}`);
-//         console.log(`\tHours = ${this._timeLeft['hours']}`);
-//         console.log(`\tMinutes = ${this._timeLeft['minutes']}`);
-//         console.log(`\tSeconds = ${this._timeLeft['seconds']}\n`);
-//     }
-
-//     decrementValues(){
-//         // get values
-//         var sec = this._secondsElement.innerHTML;
-//         var min = this._minutesElement.innerHTML;
-//         var hour = this._hourElement.innerHTML;
-//         var day = this._dayElement.innerHTML;
-//         console.log(`Beginning decrement`);
-//         // ensure that no value is less than 0 or greater than 59
-//         // for minutes and seconds, 0 should be expressed as 00
-//         // when day, hours, min, sec all equal 0, stop interval
-//         setInterval(function(day,hour,min,sec){
-//             console.log(`setInterval function`);
-
-//             if (sec > 0 ){                
-//                 if (sec - 1 === 0 ){
-//                     console.log(`\tSeconds set to 00`);
-//                     this._secondsElement.innerHTML = "00";
-//                 } else {                
-//                     sec--
-//                     this._secondsElement.innerHTML = sec;
-//                     console.log(`\tSeconds = sec`);
-//                 }
-//             } 
-//             else if ( sec === 0 || sec === '00'){
-//                 sec = 60;
-//                 sec--;
-//                 this._secondsElements.innerHTML = sec;
-//                 if (min === 0 || min === '00'){
-//                     min = 60;
-//                 }
-//                 min--;
-//                 this._minutesElement.innerHTML = min;  
-//                 console.log(`\tMinutes: ${min}, seconds = ${sec}`);
-//             }
-//             this._hourElement.innerHTML = hour;
-//             this._dayElement.innerHTML = day;
-//         }, 1000);
-//         console.log(`End of decrementValues() function`);
-//     }
-// }
-
-function decrementValues(timeLeft){
+var decrement = function decrementValues(timeLeft){
     var day = timeLeft['days'];
     var hour = timeLeft['hours'];
     var min = timeLeft['minutes'];
@@ -81,13 +12,13 @@ function decrementValues(timeLeft){
     var secElem = document.getElementById('seconds');
 
     // display remaining time
-    dayElem.innerHTML = (days === 0) ? '00' : day;
-    hourElem.innerHTML = (hour === 0) ? '00' : hour;
-    minElem.innerHTML = (min === 0) ? '00' : min;
-    secElem.innerHTML = (sec === 0) ? '00' : sec;
+    dayElem.innerHTML = (days < 10) ? '0' + hour : day;
+    hourElem.innerHTML = (hour < 10) ? '0' + hour : hour;
+    minElem.innerHTML = (min < 10) ? '0' + min : min;
+    secElem.innerHTML = (sec < 10) ? '0' + sec : sec;
 
     // start countdown and display
-    var timeInterval = 1000; // milliseconds
+    var timeInterval = 990; 
     setInterval(function(){
         console.log(`setInterval function started`);
         // decrement sec
@@ -107,7 +38,7 @@ function decrementValues(timeLeft){
         if ( sec === 59){
             --min;
             if (min < 0){
-                min = '0' + min;
+                min = 59;
             } else if (min < 10){
                 min = '0' + min;
             }
@@ -141,10 +72,16 @@ function decrementValues(timeLeft){
         dayElem.innerHTML = day;
 
         // stop interval when day = 0, hour = 0, min = 0, sec = 0
+        if (day === 0 && hour === 0 && min === 0 && sec === 0){
+            stopTimer();
+        }
 
         console.log(`Bottom of setInterval`);
     }, timeInterval);
+}
 
+function stopTimer(){
+    clearInterval(decrement);
 }
 
 
@@ -152,9 +89,11 @@ export function run(){
     $().ready(function(){
         var theBigDay = new CountDown(2021,8,10,0,0,1);
         var daysLeft = theBigDay.calculateRemainingDays();
+
+        
     
         // var count = new Counter(daysLeft);
         
-        decrementValues(daysLeft);
+        decrement(daysLeft);
     });   
 }
